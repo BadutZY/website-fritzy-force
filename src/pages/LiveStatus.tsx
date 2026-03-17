d3689b08664e12c300c75655b6f811bc1f46923d
BadutZY <rizkimputra364@gmail.com>
2026-03-17 18:43:33 +0700

    testing

--- src/pages/LiveStatus.tsx
+++ src/pages/LiveStatus.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useCallback } from "react";
 import { supabase } from "@/integrations/supabase/client";
 import fritzyProfile from "@/assets/timeline/joined-jkt48.jpg"
 
-const IDN_USERNAME = "jkt48_fritzy";
+const IDN_USERNAME = "lov_ftdhv4";
 const SHOWROOM_KEY = "JKT48_Fritzy";
 const DISPLAY_NAME = "Fritzy Rosmerian";
 const FOLLOWERS = "181.8K";
